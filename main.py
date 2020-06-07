6ddee56ca442b473010079a64fbfb274da4d5b3e
inity8 <di848588@gmail.com>
2020-06-08 00:35:57 +0300

    find parent

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 
 
-
 def main():
     file = open('index.html').read()
-    print(file)
-
+    soup = BeautifulSoup(file, 'lxml')
+    row = soup.find_all('div', {'dataset': 'salary'})
+    sweta = soup.find('div', text='Sweta').find_parent(class_='row') #or .parent.parent
+    print(sweta)
 
 
 if __name__ == '__main__':
