0d1d5fb508a10677d0eefba6e3deab56691e8665
inity8 <di848588@gmail.com>
2020-06-07 17:12:27 +0300

    create csv-writer(dict)

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def write_csv(data):
         writer.writerow((data['name'], data['surname'], data['age']))
 
 
+def write_scv2(data):
+    with open('name.csv', 'a') as file:
+        order = ['name', 'surname', 'age']
+        writer = csv.DictWriter(file, fieldnames=order)
+        writer.writerow(data)
+
+
 def main():
     d = {'name': 'Petr', 'surname': 'Ivanov', 'age': 21}
     d1 = {'name': 'Ivan', 'surname': 'Ivanov', 'age': 18}
