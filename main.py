a07c1f872a961bedfe54e0c9e6fcd3886785c4c5
inity8 <di848588@gmail.com>
2020-06-08 22:30:56 +0300

    find digits

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import re
+
     # .find()
     # .find_all()
 
@@ -19,23 +21,30 @@ def get_copywriter(tag):
     return None
 
 
+def get_salary(s):
+    # salary: 1700 usd per month
+    pattern = r'\d{1,9}'
+    # salary = re.findall(pattern, s)[0]
+    salary = re.search(pattern, s).group()      #the same as findall()
+    print(salary)
+
 def main():
     file = open('index.html').read()
     soup = BeautifulSoup(file, 'lxml')
     # row = soup.find_all('div', {'dataset': 'salary'})
     # sweta = soup.find('div', text='Sweta').find_parent(class_='row') #or .parent.parent
     # print(sweta)
-    copiwriters = []
+    # copiwriters = []
+    # persons = soup.find_all('div', class_='row')
+    # for person in persons:
+    #     sw = get_copywriter(person)
+    #     if sw:
+    #         copiwriters.append(sw)
+    # print(copiwriters)
 
-    persons = soup.find_all('div', class_='row')
-    for person in persons:
-        sw = get_copywriter(person)
-        if sw:
-            copiwriters.append(sw)
-
-    print(copiwriters)
-
-
+    salary = soup.find_all('div', {'data-set': 'salary'})
+    for i in salary:
+        get_salary(i.text)
 
 
 if __name__ == '__main__':
