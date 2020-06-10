8a97f52e70268295359726e55f708804c23e5c7c
inity8 <di848588@gmail.com>
2020-06-10 21:49:17 +0300

    add new csv

--- main-del.py
+++ main-del.py
@@ -1,53 +1,47 @@
 import csv
 import psycopg2
 from peewee import *
+import logging
 
 
-db = PostgresqlDatabase(database='test1',
+# logger = logging.getLogger('peewee')
+
+
+db = PostgresqlDatabase(database='test',
                         user='postgres',
                         password='di0',
                         host='localhost')
 
 
 class Coin(Model):
-    name = TextField()
+
+    name = CharField()
     url = TextField()
-    price = TextField()
+    price = CharField()
 
     class Meta:
         database = 'db'
 
 
 def main():
-    dict = {'name': 'Egas', 'url': 'https://coinmarketcap.com/currencies/egas/', 'price': '0.000311'}
-    data = open('1.csv')
-    for row in data:
-        print(row)
-
     # db.connect()
-    # db.create_tables([Coin, ])
+    # db.create_tables([Coin()])
 
 
+    # with open('cmc01.csv') as file:
+    #     print(file.name)
+    #     order = ['name', 'url', 'price']
+    #     reader = csv.DictReader(file, fieldnames=order)
+    #
+    #     coins = list(reader)
+    #     for i in coins:
+    #         print(i)
 
-    with open('1.csv') as file:
-        print(file.name)
-        order = ['name', 'url', 'price']
-        reader = csv.DictReader(file, fieldnames=order)
 
-        coins = list(reader)
-        for i in coins:
-            print(i)
-
-        print('***')
-
-        for i in reader:
-            print(i)
 
         # w1:
         # for row in coins:
-        #     pass
-
-            # print(row)
+        #     print(row)
         #     coin = Coin(name=row['name'],
         #                 url=row['url'],
         #                 price=row['price'])
@@ -58,6 +52,8 @@ def main():
         # with db.atomic():
         #     for row in coins:
         #         Coin.create(**row)
+
+
 
 
 if __name__ == '__main__':
