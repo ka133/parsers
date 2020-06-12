0cf01a093ae5de9acbb835688e3a150fa96da7ca
inity8 <di848588@gmail.com>
2020-06-12 17:51:55 +0300

    add multiprocessing

---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+import csv
+
+
+def get_html(url):
+    r = requests
+    return r.text
+
+
+def write_csv(data):
+    with open('websites.csv', 'a') as file:
+        order = []
+        writer = write_csv(file, fieldnames=order)
+        writer.writerow(data)
+
+
+def get_page_data(text):
+        data = text.strip().split('\n')[1:]
+
+        for row in data:
+            colums = row.strip().split('\t')
+            name = colums[0]
+            url = colums[1]
+            description = colums[2]
+            traffic = colums[3]
+            percent = colums[4]
+
+            data = {'name': name,
+                    'url': url,
+                    'description': description,
+                    'traffic': traffic,
+                    'percent': percent}
+            write_csv(data)
+
+
+def make_all(url):
+    text = get_html(url)
+    get_page_data(text)
+
+
+def main():
+    #7866
+    url = 'https://www.liveinternet.ru/rating///today.tsv?page={}'
+    urls = [url.format(str(i)) for i in range(0, 7867)]
+    print(len(urls))
+
+
+
+if __name__ == '__main__':
+    main()
+
