7e8e6b0136506adc6b681409e3fc7dc2b342e8c1
inity8 <di848588@gmail.com>
2020-06-14 23:56:12 +0300

    get json or html

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_html(url):
     r = requests.get(url)
-    return r.text
+    return r
 
 
-def main(): 
+def get_page_data(response):
+    if 'html' in response.headers['Content-Type']:
+        html = response.text
+    else:
+        html = response.json()
+    soup = BeautifulSoup(html) 
+
+
+def main():
 
     url = 'https://www.youtube.com/channel/UCY6zVRa3Km52bsBmpyQnk6A/videos'
     print(get_html(url))
