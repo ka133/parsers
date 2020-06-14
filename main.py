83c3ee549492cc0e74fe112ce7fa43acc71584c6
inity8 <di848588@gmail.com>
2020-06-15 00:00:34 +0300

    save csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 def get_html(url):
@@ -7,12 +8,28 @@ def get_html(url):
     return r
 
 
+def write_csv(data):
+    with open('videos.csv', 'a') as f:
+        order = ['name', 'url']
+        writer = csv.DictWriter(f, fieldnames=order)
+        writer.writerow(data)
+
+
 def get_page_data(response):
     if 'html' in response.headers['Content-Type']:
         html = response.text
     else:
-        html = response.json()
-    soup = BeautifulSoup(html) 
+        html = response.json()['content_html']
+    soup = BeautifulSoup(html, 'lxml')
+
+    items = soup.find_all('h3', class_='yt-lockup-title')
+
+    for item in items:
+        name = item.text.strip()
+        url = item.find('a').get('href')
+
+        data = {'name': name, 'url': url}
+        write_csv(data)
 
 
 def main():
