f8d7635248adb91e27bbf6c200bb78859ef2d3c3
inity8 <di848588@gmail.com>
2020-06-14 23:45:34 +0300

    json youtube, prepearing

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def main(): 
+
+    url = 'https://www.youtube.com/channel/UCY6zVRa3Km52bsBmpyQnk6A/videos'
+    print(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
