a60413cde80de16272e2ea69f59cd5c0c7f6323e
inity8 <di848588@gmail.com>
2020-06-15 00:38:03 +0300

    got proxy dict

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+# https://free-proxy-list.net/
+
+
+def get_html(url):
+    # proxies = {'https': 'ipaddress: 5000'}
+    r = requests.get(url)
+    return r.text
+
+
+def get_proxy(html):
+    soup = BeautifulSoup(html, 'lxml')
+    trs = soup.find('table', id='proxylisttable').find_all('tr')[1:11]  # get first 10 proxies
+    for tr in trs:
+        print(tr)
+    # .find_all('tr')[1:]
+
+    for tr in trs:
+        tds = tr.find_all('td')
+        ip = tds[0].text.strip()
+        port = tds[1].text.strip()
+        schema = 'https' if 'yes' in tds[6].text.strip() else 'http'
+
+        proxy = {'schema': schema,
+                 'address': ip + port}
+        print(proxy)
+
+
+
+def main():
+    url = 'https://free-proxy-list.net/'
+    get_proxy(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
