11b752696e29c5a3ebaa1c5925b526df8ffe0829
inity8 <di848588@gmail.com>
2020-06-16 05:12:42 +0300

    write data to csv

--- vk_app.py
+++ vk_app.py
@@ -1,6 +1,6 @@
 import requests
 import time
-
+import csv
 
 def get_count_posts():
     # https://api.vk.com/method/wall.get?access_token=a1b42744a1b42744a1b4274443a1c6b8adaa1b4a1b42744ff539e2e241551e4694a2ac9&v=5.110&domain=php2all
@@ -24,7 +24,21 @@ def get_count_posts():
         posts.extend(data)
         time.sleep(0.5)
         return posts
-    
+
+
+def file_write(posts):
+    with open('php2all', 'w') as file:
+        a_pen = csv.writer(file)
+        a_pen.writerow(('likes', 'body', 'url'))
+        for post in posts:
+            try:
+                if post['attachments'][0]['type'] == ['photo']:
+                    img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
+                else:
+                    img_url = 'pass'
+            except: pass
+            a_pen.writerow((post['likes']['count'], post['text'], img_url))
 posts = get_count_posts()
+file_write(posts)
 
 print(1)
